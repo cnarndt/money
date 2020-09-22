cc638735940195548fcfaeb9f6462396b264ea52
cnarndt <70283233+cnarndt@users.noreply.github.com>
2020-09-21 20:39:19 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::getline;
+using std::cin;
+using std::find;
+
+
+int main() {
+
+}
